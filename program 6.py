8eef0355c3717e6115f7bafa1a044562d49f22b7
Thireshsidda <92287626+Thireshsidda@users.noreply.github.com>
2021-10-30 17:15:11 +0530

    Add files via upload

---
+++ program 6.py
@@ -0,0 +1,25 @@
+m=int(input())
+if m==1:
+    print("Month 1 have 31 days")
+elif m==2:
+    print("Month 2 have 28 days")
+elif m==3:
+    print("Month 3 have 31 days")
+elif m==4:
+    print("Month 4 have 30 days")
+elif m==5:
+    print("Month 5 have 31 days")
+elif m==6:
+    print("Month 6 have 30 days")
+elif m==7:
+    print("Month 7 have 31 days")
+elif m==8:
+    print("Month 8 have 31 days")
+elif m==9:
+    print("Month 9 have 30 days")
+elif m==10:
+    print("Month 10 have 31 days")
+elif m==11:
+    print("Month 11 have 30 days")
+else:
+    print("Month 12 have 31 days")
---
+++ program5.py
@@ -0,0 +1,25 @@
+n=int(input("Enter month number"))
+if n==1:
+    print("January")
+elif n==2:
+    print("February")
+elif n==3:
+    print("March")
+elif n==4:
+    print("April")
+elif n==5:
+    print("May")
+elif n==6:
+    print("June")
+elif n==7:
+    print("July")
+elif n==8:
+    print("August")
+elif n==9:
+    print("September")
+elif n==10:
+    print("October")
+elif n==11:
+    print("November")
+else:
+    print("December")
