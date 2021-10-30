e57a458c62cc2070fe8d6c87c339ce85e31cbc9c
Thireshsidda <92287626+Thireshsidda@users.noreply.github.com>
2021-10-30 17:21:30 +0530

    Add files via upload

---
+++ program 7.py
@@ -0,0 +1,7 @@
+n=int(input())
+if n==1 or n==3 or n==5 or n==7 or n==8 or n==10 or n==12:
+    print("Month has 31 days")
+elif n==2:
+    print("Month has 28 days")
+else:
+    print("Month has 30 days")
